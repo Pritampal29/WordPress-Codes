a16cc975c0154fd087f46e75943ddf97a3bc262a
Pritam Pal <dmepritam@gmail.com>
2024-03-14 15:00:30 +0530

    Add files via upload

--- imp-codes.php
+++ imp-codes.php
@@ -345,4 +345,49 @@ while (have_posts()) : the_post();
     the_content();
 
 endwhile;
-?>
+
+
+
+/**
+ * Enable Comments in any custom post type
+ */
+function switch_on_comments_automatically(){
+    global $wpdb;
+    $wpdb->query( $wpdb->prepare("UPDATE $wpdb->posts SET comment_status = 'open'")); 
+}
+switch_on_comments_automatically();
+
+
+/**
+ * Add Views Count on Blog Post
+ */
+function gt_get_post_view() {
+    $count = get_post_meta( get_the_ID(), 'post_views_count', true );
+    return "$count";
+}
+
+function gt_set_post_view() {
+    $key = 'post_views_count';
+    $post_id = get_the_ID();
+    $count = (int) get_post_meta( $post_id, $key, true );
+    $count++;
+    update_post_meta( $post_id, $key, $count );
+}
+
+function gt_posts_column_views( $columns ) {
+    $columns['post_views'] = 'Views';
+    return $columns;
+}
+
+function gt_posts_custom_column_views( $column ) {
+    if ( $column === 'post_views') {
+        echo gt_get_post_view();
+    }
+}
+
+add_filter( 'manage_posts_columns', 'gt_posts_column_views' );
+
+add_action( 'manage_posts_custom_column', 'gt_posts_custom_column_views' ); ?>
+
+<?php gt_set_post_view(); // Add this to single-post Page ?>
+<strong><i class="fa-solid fa-eye"></i>  <?php echo gt_get_post_view(); ?></strong>  // Add this to listing Page
