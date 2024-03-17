34f7b6d8adab4178ce05898a2ebd28af3b5c10ff
Pritam Pal <dmepritam@gmail.com>
2024-03-17 21:20:56 +0530

    Add files via upload

--- woo-comm.php
+++ woo-comm.php
@@ -1,16 +1,19 @@
 <?php
 global $product;
 
+/****************************************************************************************/
 /**
  * Show Woo-Commerce Price in Pages
+ * 
  */
 
 $price = $product->get_price();
 echo $price;
 
-
+/****************************************************************************************/
 /**
  * Woo-Commerce Support in functions.php File
+ * 
  */
 
     add_theme_support("woocommerce",array(
@@ -23,15 +26,17 @@ echo $price;
         )
     ));
 
-
+/****************************************************************************************/
 /**
  * Remove Woo-Commerce Sidebar From Shop Page
+ * 
  */
     remove_action('woocommerce_sidebar','woocommerce_get_sidebar');
 
-
+/****************************************************************************************/
 /**
  * Open Extra Div before Content & Close The Same div After Main Content in functions.php file
+ * 
  */
 
 	function open_extra_div() {
@@ -44,4 +49,62 @@ echo $price;
 	}
 	add_action('woocommerce_after_main_content','close_extra_div',5);
  	
+/****************************************************************************************/
+/**
+ * Open Class on Woo-Commerce Sidebar in functions.php file
+ * 
+ */
 
+    function open_sidebar_column_grid() {
+        echo "<div class='col-md-4'>";
+    }
+    add_action('woocommerce_before_main_content','open_sidebar_column_grid',6);
+
+
+
+    // Add Sidebar In-Between those Div
+    add_action('woocommerce_before_main_content','woocommerce_get_sidebar',7);
+
+
+    /**
+     * Close Class on Woo-Commerce Sidebar in functions.php file
+     */
+
+    function close_sidebar_column_grid() {
+        echo "</div>";
+    }
+    add_action('woocommerce_before_main_content','close_sidebar_column_grid',8);
+
+/****************************************************************************************/
+/**
+ * Open Class on Woo-Commerce Main Content in functions.php file
+ * 
+ */
+
+    function open_product_column_grid() {
+        echo "<div class='col-md-8'>";
+    }
+    add_action('woocommerce_before_main_content','open_product_column_grid',9);
+
+
+    /**
+     * Close Class on Woo-Commerce Main Content in functions.php file
+     */
+
+    function close_product_column_grid() {
+        echo "</div>";
+    }
+    add_action('woocommerce_before_main_content','close_product_column_grid',10);
+
+/****************************************************************************************/
+/**
+ * In Single Product Page If Need to Show full Width Page without Sidebar
+ * 
+ */
+
+    add_action('template_redirect','load_template_layout');
+    function load_template_layout() {
+        if(is_shop()) {
+            // Add Open Class on Woo-Commerce Sidebar & Open Class on Woo-Commerce Main Content Code Here
+        }
+    }
