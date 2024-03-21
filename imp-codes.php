8ac70c3e806bed006bbad57114fcfe0fee5f6c78
Pritam Pal <dmepritam@gmail.com>
2024-03-21 18:58:57 +0530

    Add files via upload

--- imp-codes.php
+++ imp-codes.php
@@ -6,6 +6,32 @@
  */
 ?>
 
+<?php
+/** 
+ * Remove WordPress version number from scripts and styles
+ */ 
+function remove_wp_version_strings( $src ) {
+    global $wp_version;
+    parse_str(parse_url($src, PHP_URL_QUERY), $query);
+    if ( !empty($query['ver']) && $query['ver'] === $wp_version ) {
+        $src = remove_query_arg('ver', $src);
+    }
+    return $src;
+}
+add_filter('script_loader_src', 'remove_wp_version_strings');
+add_filter('style_loader_src', 'remove_wp_version_strings');
+
+/** 
+ * Remove WordPress version number & Plugin version from scripts and styles
+ */ 
+function remove_script_version( $src ) {
+    return remove_query_arg( 'ver', $src );
+}
+add_filter( 'script_loader_src', 'remove_script_version' );
+add_filter( 'style_loader_src', 'remove_script_version' );
+
+?>
+
 /**
 * Disable add new Plugin option on dashboard
 */
